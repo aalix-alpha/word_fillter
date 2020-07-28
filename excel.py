457e9cdfaa5aa12584e72df1e20b18d4d9f1fe20
A. Alix <64742295+aalix-alpha@users.noreply.github.com>
2020-07-28 20:57:45 +0530

    Add files via upload

---
+++ excel.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+
+location = input("Enter location to scan\n=> ")
+os.chdir(location)
+for (root, dir, files) in os.walk(location):
+    for file1 in range(len(files)):
+        try:
+            if ".xlsx" in files[file1]:
+
+                df = pd.read_excel(os.path.join(os.path.abspath(root), files[file1]))
+                for file2 in range(len(files)):
+                    if ".xlsx" in files[file2] and file1!=file2:
+                        df2 = pd.read_excel(os.path.join(os.path.abspath(root), files[file2 ]))
+
+                        if len(df.columns)==len(df2.columns) and len(df.index)==len(df2.index):
+                            os.remove(os.path.join(os.path.abspath(root), files[file1]))
+        except Exception as e:
+            print("%s: %s"%(e.__class__.__name__, e))
+            print("File Deleted!", os.path.join(os.path.abspath(root), files[file1]))
+
